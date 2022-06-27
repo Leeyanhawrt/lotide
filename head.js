9b71bb884e6fe5c59187f35ef503a84a540cc79c
Leeyan Haw <leeyan1108@gmail.com>
2022-06-27 18:25:42 +0000

    Added head function

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (arr) => {
+  return arr.shift();
+}
+
+assertEqual(head([]), 5);
