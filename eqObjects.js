da876120155061b548d7f774cb89a1511a4fb2f9
Leeyanhawrt <leeyan1108@gmail.com>
2022-06-29 20:18:05 +0000

    Add eqobject to lotide

---
+++ eqObjects.js
@@ -0,0 +1,57 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("hello", "hello");
+
+let eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false
+    }
+  }
+  return true;
+};
+
+// Returns true if both objects inputted are equal
+const eqObjects = function (obj1, obj2) {
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+
+  for (const key of obj1Keys) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false
+      }
+    }
+    else if (obj1[key] !== obj2[key]) {
+      return false
+    }
+  }
+  return true;
+}
+
+
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
+
+// console.log(eqObjects({ a: 1, b: 2, c: 2 }, { a: 1, b: 2, c: 5}))
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
