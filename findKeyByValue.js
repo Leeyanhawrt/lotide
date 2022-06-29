6763b6659670ba7377335161bedd24d222acfccc
Leeyanhawrt <leeyan1108@gmail.com>
2022-06-29 18:37:33 +0000

    Add findKeyByValue to lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  for (const genre in obj) {
+    if (obj[genre] === val) {
+      return genre;
+    }
+  }
+  return undefined;
+};
+
+// assertEqual("hello", "hello");
+
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama: "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
