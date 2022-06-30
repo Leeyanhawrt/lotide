567383337934f534fcd13d846cae2b301d0aadc9
Leeyanhawrt <leeyan1108@gmail.com>
2022-06-30 19:51:12 +0000

    Add map to lotide

---
+++ map.js
@@ -0,0 +1,11 @@
+let map = (arr, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+console.log(map(words, word => word[0]))
