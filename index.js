ef13168689edb87fef7c50cd22800c500ed0f530
Leeyanhawrt <leeyan1108@gmail.com>
2022-07-04 13:40:57 +0000

    Add ES6 object short hand to index

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const assertEqual = require('./assertEqual');
 
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
 };
