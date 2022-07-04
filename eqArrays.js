07d7e304e7647442c4e0223b2053ff9573ae8d55
Leeyanhawrt <leeyan1108@gmail.com>
2022-07-04 12:11:16 +0000

    Refactor eqArrays to include module

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 let eqArrays = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
@@ -14,3 +6,5 @@ let eqArrays = (arr1, arr2) => {
   }
   return true;
 };
+
+module.exports = eqArrays;
