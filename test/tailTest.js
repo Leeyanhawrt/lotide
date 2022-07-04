2684657cd7844302d70d51ba34a2f6ab910151ee
Leeyanhawrt <leeyan1108@gmail.com>
2022-07-04 12:59:29 +0000

    Add mocha and chai assertion to tail and middle

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
 const assertEqual = require('../assertEqual')
 const tail = require('../tail')
+const assert = require('chai').assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs'", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"])
+  });
+
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3])
+  })
+});
